ade69003b2b39eaf66e6051cbddb3a31283bd21d
Your Name <you@example.com>
2025-03-01 14:29:25 +0200

    modifications was added to count func

--- Dic.py
+++ Dic.py
@@ -8,9 +8,19 @@ def count_word_freq(sent):
     #split sentence provided
     words=sent.split()
     freq_dict={} #create empty dict
-    
+    for word in words:
+        if word in freq_dict:
+            freq_dict[word]+=1
+        else:
+            freq_dict[word]=1
+    return freq_dict
+
 
 dict1={"a":1,"b":2,"c":3}
 dict2={"b":10,"d":4}
 
 print(merge_dictionaries(dict1,dict2))
+
+
+sent="hello my name is hady, hady is my first name , while my last name is kaddourah"
+print(count_word_freq(sent))
