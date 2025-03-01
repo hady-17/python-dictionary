6c63befa6212693661f66f227e59fddf563bbc62
Your Name <you@example.com>
2025-03-01 14:25:40 +0200

    count func created

--- Dic.py
+++ Dic.py
@@ -4,6 +4,11 @@ def merge_dictionaries(dic1,dic2):
     #update with the second dictionary
     merged.update(dic2)
     return merged
+def count_word_freq(sent):
+    #split sentence provided
+    words=sent.split()
+    freq_dict={} #create empty dict
+    
 
 dict1={"a":1,"b":2,"c":3}
 dict2={"b":10,"d":4}
