dc1aa05768a136e24b6669edcc9a883334df6d30
Your Name <you@example.com>
2025-03-01 14:23:13 +0200

    merge func created

--- Dic.py
+++ Dic.py
@@ -1,2 +1,11 @@
+def merge_dictionaries(dic1,dic2):
+    #create a copy of the first dictionary
+    merged=dic1.copy()
+    #update with the second dictionary
+    merged.update(dic2)
+    return merged
+
 dict1={"a":1,"b":2,"c":3}
 dict2={"b":10,"d":4}
+
+print(merge_dictionaries(dict1,dict2))
